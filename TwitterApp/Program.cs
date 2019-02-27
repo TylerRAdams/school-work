c57926c91b9e05141f07791a60dd049bd094073f
Tyler Adams <32653655+TylerAdamsUNCO@users.noreply.github.com>
2019-02-27 12:47:30 -0700

    Adding files

---
+++ TwitterApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Tweetinvi;
+
+namespace TwitterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auth.SetUserCredentials("6RyameaQdHb58UWgeIdRcJpzJ", "PhfqFS92SuEYO6mN2ru37vhQgmiRsvgJw2O1NtAn7ZLtXvA7gD", "	915239441806929921-grOFuq8pprynA0aiAY1xAJYTpoYyDV1", "m3QDkIREbBs0Zz2AOAxLYVQXF14KUGfbxg5pmc7y7TqKq");
+
+            var user = User.GetAuthenticatedUser();
+            Console.WriteLine(user);
+
+            var tweet = Tweet.PublishTweet("Hello from Tweetinvi!");
+
+            var timelineTweets = Timeline.GetUserTimeline(user, 5);
+
+            foreach (var timelineTweet in timelineTweets)
+            {
+                Console.WriteLine(timelineTweet);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
