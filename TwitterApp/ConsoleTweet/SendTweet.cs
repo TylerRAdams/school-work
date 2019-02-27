5e723fc742d8edad3cd62289d8b0890518b330e5
Tyler Adams <32653655+TylerAdamsUNCO@users.noreply.github.com>
2019-02-27 12:48:54 -0700

    Added more files

---
+++ TwitterApp/ConsoleTweet/SendTweet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi
+
+namespace ConsoleTweet
+{
+    class SendTweet
+    {
+        public void TweetFunction(string tweetMessage)
+        {
+         
+                Auth.SetUserCredentials("6RyameaQdHb58UWgeIdRcJpzJ", "PhfqFS92SuEYO6mN2ru37vhQgmiRsvgJw2O1NtAn7ZLtXvA7gD", "915239441806929921-grOFuq8pprynA0aiAY1xAJYTpoYyDV1", "m3QDkIREbBs0Zz2AOAxLYVQXF14KUGfbxg5pmc7y7TqKq");
+
+                var user = User.GetAuthenticatedUser();
+
+                Tweet.PublishTweet(tweetMessage);
+
+                var timelineTweets = Timeline.GetUserTimeline(user, 5);            
+        }
+    }
+}
+
