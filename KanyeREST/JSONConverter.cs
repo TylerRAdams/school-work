d8169a64b11866e9869c71bb75f5c68304d0bb90
Tyler Adams <32653655+TylerAdamsUNCO@users.noreply.github.com>
2019-05-09 23:14:52 -0600

    Add files via upload

---
+++ KanyeREST/JSONConverter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeREST
+{
+    class JSONConverter
+    {
+        /*
+         * MS: Great example of encapsulating both the parameter and the return 
+         * variable.
+         */
+        public Song Convert(JsonInput input)
+        {
+                Song thisSong = JsonConvert.DeserializeObject<Song>(input.Json);
+                return thisSong;
+        }
+    }
+}
+
---
+++ KanyeREST/Request.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeREST
+{
+    class Request
+    {
+        /*
+         * MS: This is where you could / should have encapsulated the title 
+         * as well as the response here rather than passing in and returning a string. 
+         * It's admittedly overkill... but you could imagine passing in an object with a title
+         * that gets the response populated.  Or you could set a property on the Request... 
+         * any number of ways. 
+         */
+        public String GetResponse(string title)
+        {
+            WebClient client = new WebClient();
+            String downloadedString = client.DownloadString("http://www.kanyerest.xyz/api/track/" + title);
+            return downloadedString;
+        }
+    }
+}
